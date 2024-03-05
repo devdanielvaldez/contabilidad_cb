b3cfc83c49849cdcea0dbab1a333d5ea9d64079d
DANIEL VALDEZ GUZMAN, BHD <daniel_valdezguzman@bhd.com.do>
2024-03-05 10:52:34 -0400

    fix in db

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ const config = {
   db: {
     /* don't expose password or any sensitive info, done only for demo */
     host: "31.170.167.1",
-    user: "u675638840_admin",
-    password: "Prueba01*",
-    database: "u675638840_colegio_buenos",
+    user: "u675638840_admin1",
+    password: "Admin_1234@",
+    database: "u675638840_colegiobuenosa",
   },
   listPerPage: 10,
 };
